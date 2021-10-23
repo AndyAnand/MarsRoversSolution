784a09ce84e21b601366224126e0a13da62d3c1c
andygajadhar <andygajadhar@gmail.com>
2021-10-23 15:16:24 -0300

    Add MarsTerrain class and unit tests

---
+++ src/MarsRoversSolution.Domain/Models/MarsTerrain.cs
@@ -0,0 +1,22 @@
+﻿using Ardalis.GuardClauses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarsRoversSolution.Domain.Models
+{
+    /// <summary>
+    /// Will delimit the area where a rover can move
+    /// </summary>
+    public class MarsTerrain
+    {
+        public MarsTerrain(int width, int height)
+        {
+            Height = Guard.Against.NegativeOrZero(height, nameof(height));
+            Width = Guard.Against.NegativeOrZero(width, nameof(width));
+        }
+
+        public int Height { get; private set; }
+        public int Width { get; private set; }
+    }
+}
---
+++ tests/MarsRoversSolution.Tests/MarsTerrainTests.cs
@@ -0,0 +1,41 @@
+﻿using MarsRoversSolution.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MarsRoversSolution.Tests
+{
+    public class MarsTerrainTests
+    {
+        [Fact]
+        public void CreatingWithPositiveDimensionsShouldBeValid()
+        {
+            var width = 5;
+            var heigth = 5;
+            var marsTerrain = new MarsTerrain(width, heigth);
+
+            Assert.True(marsTerrain.Width == width);
+            Assert.True(marsTerrain.Height == heigth);
+        }
+
+        [Fact]
+        public void CreatingWithNegativeDimensionsShouldThrowException()
+        {
+            var width = -5;
+            var heigth = 5;
+
+            Assert.Throws<ArgumentException>(() => new Position(width, heigth));
+
+            width = -5;
+            heigth = 5;
+            Assert.Throws<ArgumentException>(() => new Position(width, heigth));
+
+            width = -5;
+            heigth = -5;
+            Assert.Throws<ArgumentException>(() => new Position(width, heigth));
+        }
+    }
+}
