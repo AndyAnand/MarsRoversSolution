2017e67c81705b53951407aa61024d212757229a
andygajadhar <andygajadhar@gmail.com>
2021-10-23 17:14:02 -0300

    Fix Direction Enum namespace

--- src/MarsRoversSolution.Domain/Enums/Direction.cs
+++ src/MarsRoversSolution.Domain/Enums/Direction.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace MarsRoversSolution.Domain.Interfaces
+namespace MarsRoversSolution.Domain.Enums
 {
     public enum Direction
     {
