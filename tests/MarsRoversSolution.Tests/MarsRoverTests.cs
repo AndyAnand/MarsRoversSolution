0e5ad248f21940dfc4bfffdea4879ca9bdf8a5f6
andygajadhar <andygajadhar@gmail.com>
2021-10-23 16:19:44 -0300

    Unit testing the creation of Mars Rovers

---
+++ tests/MarsRoversSolution.Tests/MarsRoverTests.cs
@@ -0,0 +1,38 @@
+﻿using MarsRoversSolution.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MarsRoversSolution.Tests
+{
+    public class MarsRoverTests
+    {
+        [Fact]
+        public void PositionIsRequired()
+        {
+            Position nullPosition = null;
+            var marsTerrain = new MarsTerrain(5, 5);
+
+            Assert.Throws<ArgumentNullException>(() 
+                => new MarsRover(marsTerrain, nullPosition, Domain.Enums.Heading.East));
+        }
+
+        [Fact]
+        public void TerrainIsRequired()
+        {
+            var position = new Position(2,3);
+            MarsTerrain nullTerrain = null;
+
+            Assert.Throws<ArgumentNullException>(()
+                => new MarsRover(nullTerrain, position, Domain.Enums.Heading.East));
+        }
+
+        public void ShouldHaveAlwaysAStateForEachHeadingValue()
+        {
+
+        }
+    }
+}
