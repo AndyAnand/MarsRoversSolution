b106758627dde4ce905e1e1808c4371afd0905dc
andygajadhar <andygajadhar@gmail.com>
2021-10-23 15:08:46 -0300

    Add Position Model + UnitTests

---
+++ src/MarsRoversSolution.Domain/Models/Position.cs
@@ -0,0 +1,46 @@
+﻿using Ardalis.GuardClauses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarsRoversSolution.Domain.Models
+{
+    /// <summary>
+    /// A position on Mars given by positive East and North coordinates (equivalent to
+    /// the first quadrant of Cartesian Plane)
+    /// </summary>
+    public class Position
+    {
+        public Position(int eastUnits, int northUnits)
+        {
+            EastUnits = Guard.Against.Negative(eastUnits, nameof(eastUnits));
+            NorthUnits = Guard.Against.Negative(northUnits, nameof(northUnits));
+        }
+
+        private int _eastUnits;
+        public int EastUnits
+        {
+            get
+            {
+                return _eastUnits;
+            }
+            set
+            {
+                _eastUnits = Guard.Against.Negative(value, nameof(value));
+            }
+        }
+
+        private int _northUnits;
+        public int NorthUnits
+        {
+            get
+            {
+                return _northUnits;
+            }
+            set
+            {
+                _northUnits = Guard.Against.Negative(value, nameof(value));
+            }
+        }
+    }
+}
---
+++ tests/MarsRoversSolution.Tests/PositionTests.cs
@@ -0,0 +1,41 @@
+﻿using MarsRoversSolution.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MarsRoversSolution.Tests
+{
+    public class PositionTests
+    {
+        [Fact]
+        public void CreatingWithPositiveCoordinatesShouldBeValid()
+        {
+            var eastUnits = 5;
+            var northUnits = 5;
+            var position = new Position(eastUnits, northUnits);
+
+            Assert.True(position.EastUnits == eastUnits);
+            Assert.True(position.NorthUnits == northUnits);
+        }
+
+        [Fact]
+        public void CreatingWithNegativeCoordinatesShouldThrowException()
+        {
+            var eastUnits = -5;
+            var northUnits = 5;
+
+            Assert.Throws<ArgumentException>(() => new Position(eastUnits, northUnits));
+
+            northUnits = -5;
+            eastUnits = 5;
+            Assert.Throws<ArgumentException>(() => new Position(eastUnits, northUnits));
+
+            northUnits = -5;
+            eastUnits = -5;
+            Assert.Throws<ArgumentException>(() => new Position(eastUnits, northUnits));
+        }
+    }
+}
